6240ef5a5e87510876a3adb11cb63b6b1e6e8518
sagreenxyz <sagreenxyz@gmail.com>
2025-03-28 08:19:07 -0500

    waypoint

--- static/chapter-28a.svelte
+++ static/chapter-28a.svelte
@@ -1,6 +1,205 @@
 <section>
-	<h2>Chapter 28 - Questions</h2>
-	<ol>
-		<!-- Add questions here -->
-	</ol>
+    <h2>Chapter 28 - Questions</h2>
+    <ol>
+        <li>
+            <p>Sperm must first complete ________ to enable the fertilization of an oocyte.</p>
+            <ul>
+                <li>the fast block</li>
+                <li>the acrosomal reaction</li>
+                <li>capacitation</li>
+                <li>the cortical reaction</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> capacitation</p>
+        </li>
+        <li>
+            <p>Progesterone secreted by the placenta suppresses ________ to prevent maturation of ovarian follicles.</p>
+            <ul>
+                <li>FSH and LH</li>
+                <li>hCG and FSH</li>
+                <li>estrogen and hCG</li>
+                <li>LH and estrogen</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> FSH and LH</p>
+        </li>
+        <li>
+            <p>Cleavage produces daughter cells called ________.</p>
+            <ul>
+                <li>blastomeres</li>
+                <li>morulae</li>
+                <li>trophoblasts</li>
+                <li>blastocysts</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> blastomeres</p>
+        </li>
+        <li>
+            <p>The conceptus, upon reaching the uterus, first ________.</p>
+            <ul>
+                <li>implants</li>
+                <li>hatches</li>
+                <li>divides</li>
+                <li>disintegrates</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> divides</p>
+        </li>
+        <li>
+            <p>A __________________ is a term for the conceptus at the developmental stage that consists of about 100 cells shaped into an inner cell mass that is fated to become the embryo and an outer trophoblast that is fated to become the associated fetal membranes and placenta.</p>
+            <ul>
+                <li>blastocoel</li>
+                <li>blastomere</li>
+                <li>blastocell</li>
+                <li>blastocyst</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> blastocyst</p>
+        </li>
+        <li>
+            <p>The inner cell mass of the blastocyst is destined to become the ________.</p>
+            <ul>
+                <li>trophoblast</li>
+                <li>embryo</li>
+                <li>placenta</li>
+                <li>chorionic villi</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> embryo</p>
+        </li>
+        <li>
+            <p>Fusion of pronuclei occurs during ________.</p>
+            <ul>
+                <li>capacitation</li>
+                <li>spermatogenesis</li>
+                <li>ovulation</li>
+                <li>fertilization</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> fertilization</p>
+        </li>
+        <li>
+            <p>Which primary germ layer gave rise to the cells that eventually became the central nervous system?</p>
+            <ul>
+                <li>endoderm</li>
+                <li>acrosome</li>
+                <li>mesoderm</li>
+                <li>ectoderm</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> ectoderm</p>
+        </li>
+        <li>
+            <p>What would happen if the trophoblast did not secrete hCG upon implantation of the blastocyst?</p>
+            <ul>
+                <li>Menses would flush the blastocyst out of the uterus.</li>
+                <li>The uterine mucosa would not envelop the blastocyst.</li>
+                <li>The corpus luteum would continue to produce progesterone and estrogen.</li>
+                <li>The cells would not continue to divide.</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> Menses would flush the blastocyst out of the uterus.</p>
+        </li>
+        <li>
+            <p>During what process does the amnion envelop the embryo?</p>
+            <ul>
+                <li>embryonic folding</li>
+                <li>implantation</li>
+                <li>gastrulation</li>
+                <li>organogenesis</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> embryonic folding</p>
+        </li>
+        <li>
+            <p>Mature breast milk _____________.</p>
+            <ul>
+                <li>has more fat than cow’s milk</li>
+                <li>has more sodium than cow’s milk</li>
+                <li>has more protein than cow’s milk</li>
+                <li>has more calcium than cow’s milk</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> has more fat than cow’s milk</p>
+        </li>
+        <li>
+            <p>Marfan syndrome is inherited in an autosomal dominant pattern. Which of the following is true?</p>
+            <ul>
+                <li>Female offspring are more likely to inherit the disease.</li>
+                <li>Male offspring are more likely to inherit the disease.</li>
+                <li>Female offspring are more likely to be carriers of the disease.</li>
+                <li>Male and female offspring have the same likelihood of inheriting the disease.</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> Male and female offspring have the same likelihood of inheriting the disease.</p>
+        </li>
+        <li>
+            <p>The foramen ovale causes the fetal circulatory system to bypass the ________.</p>
+            <ul>
+                <li>lungs</li>
+                <li>kidneys</li>
+                <li>gonads</li>
+                <li>liver</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> lungs</p>
+        </li>
+        <li>
+            <p>In addition to codominance, the ABO blood group antigens are also an example of ___________.</p>
+            <ul>
+                <li>recessive lethal inheritance</li>
+                <li>X-linked recessive inheritance</li>
+                <li>multiple alleles</li>
+                <li>incomplete dominance</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> multiple alleles</p>
+        </li>
+        <li>
+            <p>What happens to the urine excreted by the fetus when the kidneys begin to function?</p>
+            <ul>
+                <li>The umbilical cord carries it to the placenta for removal.</li>
+                <li>It is turned into meconium.</li>
+                <li>The endometrium absorbs it.</li>
+                <li>It adds to the amniotic fluid.</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> It adds to the amniotic fluid.</p>
+        </li>
+        <li>
+            <p>What terms describes the implantation of an embryo outside of the uterus?</p>
+            <ul>
+                <li>peritopic pregnancy</li>
+                <li>endopic pregnancy</li>
+                <li>ectopic pregnancy</li>
+                <li>etopic pregnancy</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> ectopic pregnancy</p>
+        </li>
+        <li>
+            <p>During weeks 9–12 of fetal development, ________.</p>
+            <ul>
+                <li>surfactant production begins in the fetal lungs</li>
+                <li>the spinal cord begins to be myelinated</li>
+                <li>meconium begins to accumulate in the intestines</li>
+                <li>bone marrow begins to assume erythrocyte production</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> bone marrow begins to assume erythrocyte production</p>
+        </li>
+        <li>
+            <p>The placenta is formed from ________.</p>
+            <ul>
+                <li>the mother’s endometrium only</li>
+                <li>the embryo’s mesenchymal cells</li>
+                <li>the mother’s endometrium and the embryo’s chorionic membrane</li>
+                <li>the mother’s endometrium and the embryo’s umbilical cord</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> the mother’s endometrium and the embryo’s chorionic membrane</p>
+        </li>
+        <li>
+            <p>Which of the following is a possible culprit of “morning sickness”?</p>
+            <ul>
+                <li>increased minute respiration</li>
+                <li>increased blood volume</li>
+                <li>decreased intestinal peristalsis</li>
+                <li>decreased aldosterone secretion</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> decreased intestinal peristalsis</p>
+        </li>
+        <li>
+            <p>How does the decrease in progesterone at the last weeks of pregnancy help to bring on labor?</p>
+            <ul>
+                <li>stimulating FSH production</li>
+                <li>decreasing the inhibition of uterine contractility</li>
+                <li>decreasing the levels of estrogens</li>
+                <li>dilating the cervix</li>
+            </ul>
+            <p><strong>Correct Answer:</strong> decreasing the inhibition of uterine contractility</p>
+        </li>
+    </ol>
 </section>
